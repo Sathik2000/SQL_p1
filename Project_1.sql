569b081f87c623180a93a6f914ca229fed899296
Sathik2000 <125662651+Sathik2000@users.noreply.github.com>
2023-04-06 18:54:02 +0530

    Add files via upload

---
+++ Project_1.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employee_db.employees;
+/**step-3**/
+select * from employee_db.employees;
+/**step-4**/
+update employee_db.employees set Salary=90000.00 where ID=4;
+/**step-5**/
+delete FROM employee_db.employees where ID=3;
+
